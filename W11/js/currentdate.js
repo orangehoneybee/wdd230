bf07e352e844c63fdea00a2e1f2f9b836189cb17
Finn Carver <needanotheremail14543@gmail.com>
2022-03-21 08:31:28 -0600

    nav bar fixes hopefully

---
+++ W11/js/currentdate.js
@@ -0,0 +1,14 @@
+
+const options = {weekday: 'long' , day: 'numeric', month: 'long', year: 'numeric'};
+const year = {year: 'numeric'}
+document.getElementById('year').textContent = new Date().toLocaleDateString('en-us', year);
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-us', options);
+
+// special msg for mon and tues
+
+  var currentDate = new Date();
+  weekdayValue = currentDate.getDay();
+if (weekdayValue == 1 || weekdayValue ==2){
+  spclmsg = "🤝🏼Come join us for the chamber meet and greet on Conthsen 2 hours past sunset";
+}
+document.querySelector('#spclmsg').textContent = spclmsg;
--- W11/js/script.js
+++ W11/js/script.js
@@ -1,30 +1,7 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-const options = {weekday: 'long' , day: 'numeric', month: 'long', year: 'numeric'};
-const year = {year: 'numeric'}
-document.getElementById('year').textContent = new Date().toLocaleDateString('en-us', year);
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-us', options);
-
- function toggleMenu() {
-   document.getElementById("primaryNav").classList.toggle("open");
-   document.getElementById("ham").classList.toggle("open");
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("ham").classList.toggle("open");
 }
 
 const x = document.getElementById("ham");
 x.onclick = toggleMenu;
-
-// special msg for mon and tues
-
-  var currentDate = new Date();
-  weekdayValue = currentDate.getDay();
-if (weekdayValue == 1 || weekdayValue ==2){
-  spclmsg = "🤝🏼Come join us for the chamber meet and greet on Conthsen 2 hours past sunset";
-}
-document.querySelector('#spclmsg').textContent = spclmsg;
